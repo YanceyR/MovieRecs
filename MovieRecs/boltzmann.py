6704ff5a8e910cc7842fc63830c1eca2c5c729ec
Yancey Reid <Yancey.Reid@gmail.com>
2018-10-30 22:00:18 -0400

    complete RBM class

--- MovieRecs/boltzmann.py
+++ MovieRecs/boltzmann.py
@@ -38,19 +38,19 @@ totalMovies = max(max(trainingSet[:, movieCol]), max(testSet[:, movieCol]))
 def convert(data):
     converted_data = []
     for id_users in range(1, totalUsers + 1):
-        
+
         # second [] is a conditional, ndarray
         id_movies = data[:, movieCol][data[:,0] == id_users]
         id_ratings = data[:, ratingCol][data[:,0] == id_users]
         ratings = numpy.zeros(totalMovies)
-                
+
         # array indexing is possible because of numpy, type is ndarray
         ratings[id_movies - 1] = id_ratings
         converted_data.append(ratings)
-        
+
     return converted_data
-        
-        
+
+
 trainingSet = convert(trainingSet)
 testSet = convert(testSet)
 
@@ -73,39 +73,46 @@ class RBM():
     def __init__(self, visible_nodes_count, hidden_nodes_count):
         self.visible_nodes_count = visible_nodes_count
         self.hidden_nodes_count = hidden_nodes_count
-        
+
         # weights are initialized with random values with normal distribution
         self.weights = torch.randn(visible_nodes_count, hidden_nodes_count)
-        
+
         # tensor functions dont accept single dim tensors
         self.hidden_bias = torch.randn(1, hidden_nodes_count)
         self.visible_bias = torch.randn(1, visible_nodes_count)
-        
+
         # get hidden neurons that were activated according to p (hidden | visible)
         def get_hidden_sample(self, visible_neurons):
             weight_x_neurons = torch.mm(visible_neurons, self.weights.t())
-            
-            # make sure bias is applied to each row in wn
+
+            # make sure bias is applied to each row in weight_x_neurons
             activation_func = weight_x_neurons + self.hidden_bias.expand_as(weight_x_neurons)
-            
+
             # Each element corresponds to each hidden node.
             # Each element is the probability that the hidden node is activated.
             prob_hidden_given_visible = torch.sigmoid(activation_func)
-            
+
             # sample of hidden neurons usng Bernoulli sampling,
             # generate random val between 0 and 1.
             # If val <= probability, neuron is activated i.e cell == 1, else 0
             hidden_sample = torch.bernoulli(prob_hidden_given_visible)
-            
+
             return prob_hidden_given_visible, hidden_sample
+
+        # get visible neurons that were activated according to p (visible | hidden)
+        def get_visible_sample(self, hidden_neurons):
+            weight_x_neurons = torch.mm(hidden_neurons, self.weights)
+            activation_func = weight_x_neurons + self.visible_bias.expand_as(weight_x_neurons)
+            prob_visible_given_hidden = torch.sigmoid(activation_func)
+            visible_sample = torch.bernoulli(prob_visible_given_hidden)
+            return prob_visible_given_hidden, visible_sample
+
+        # compute gradient to minimize energy
+        # k-step contrastive divergence
+        # k signifies after k sampling
+        def train(self, ratings_per_user, ratings_per_user_k, prob_hidden, prob_hidden_k):
+            self.weights += torch.mm(ratings_per_user.t(), prob_hidden) - torch.mm(ratings_per_user_k.t(), prob_hidden_k)
+            self.visible_bias += torch.sum((ratings_per_user - ratings_per_user_k), 0)
+            self.hidden_bias += torch.sum((prob_hidden - prob_hidden_k), 0)
+
             
-            
-            
-
-
-
-
-
-
-
-
